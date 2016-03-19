ae275df2c2d55b9bdf20504833a11ad849b4df9c
winmine <winmine@zheng-mengyuandeMacBook-Air.local>
2016-03-19 22:58:08 +0800

    java_1

---
+++ 狗的类/src/dog_class.java
@@ -0,0 +1,18 @@
+
+public class dog_class {
+	
+
+
+	   String weight;
+	   int age;
+	   double 
+	   String color;
+	   void barking(){
+	   }
+	   
+	   void hungry(){
+	   }
+	   
+	   void sleeping(){
+	   }
+	}
