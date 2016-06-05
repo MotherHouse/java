963cf7b214cbe90981743fc949915a1454614bfb
winmine <winmine@zheng-mengyuandeMacBook-Air.local>
2016-06-05 20:21:46 +0800

    输出开头为b的字符

---
+++ 输出b开头的字符串/src/printf_first_b.java
@@ -0,0 +1,13 @@
+public class printf_first_b {
+	
+public static void main(String args[]){
+
+
+String [] winmine = new String[]{"owlcity","bilibili","","cube","blood"};
+for(int i=0;i<winmine.length;i++){
+   if(winmine[i].startsWith("b")){
+      System.out.println(winmine[i]);
+   }
+}
+}
+}
